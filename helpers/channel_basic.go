30a9054415f4ad458c9563faa779b8dea3fc7246
= <bibekkarki662@gmail.com>
2023-03-14 23:08:44 +0545

    iterative send and receving the data from channel

--- helpers/channel_basic.go
+++ helpers/channel_basic.go
@@ -8,25 +8,27 @@ import (
 func BasicChannel(wg *sync.WaitGroup) {
 	channel := make(chan int)
 	wg.Add(2)
+
 	//receiver goutine function
-	go func() {
+	go func(ch <-chan int) {
 		defer wg.Done()
-		//received value from channel
 
-		i := <-channel
-		fmt.Println("received value: ", i)
-		//send new value back to channel
-		channel <- 10
+		for i := range ch {
+			fmt.Println("received value: ", i)
+		}
 
-	}()
+	}(channel)
 
 	//sender goroutine function
-	go func() {
+	go func(ch chan<- int) {
 		defer wg.Done()
 		//send 42 to channel
-		channel <- 42
-		fmt.Println("received value: ", <-channel)
+		for j := 0; j <= 50; j++ {
+			ch <- j
+		}
+		close(ch)
 
-	}()
+	}(channel)
+
 	wg.Wait()
 }
